014602805d239fd6477181c0573e0a93c9690e87
Matheus Afonso <matheusafonsouza@gmail.com>
2022-12-04 13:30:15 -0300

    feat: queue module
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>

---
+++ src/queue.h
@@ -0,0 +1,14 @@
+#ifndef QUEUE
+#define QUEUE
+
+
+typedef struct TNode {
+  int id;
+  char buffer[200];
+  int len;
+  struct TNode* next;
+} TNode;
+int insert(int id, const char * chunck, int len, struct TNode **ptr_init);
+int removeList(struct TNode **ptr_init);
+
+#endif
